02db8bcf4fa69e23266b7cbaea7a331e12e84b82
Giovanni Panascia <giovanni.panascia@7e3.it>
2019-12-12 11:16:49 +0100

    initial

---
+++ MarioProva/MyObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace MarioProva
+{
+    public class MyObject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class MyList
+    {
+        public List<MyObject> List { get; } = new List<MyObject>
+        {
+            new MyObject
+            {
+                Id = 1,
+                Name = "Mario",
+                Surname = "Fagiolo",
+                Number = 33
+            },
+            new MyObject
+            {
+                Id = 2,
+                Name = "Giovanni",
+                Surname = "Panascia",
+                Number = 37
+            },
+            new MyObject
+            {
+                Id = 3,
+                Name = "Damiano",
+                Surname = "Bisecco",
+                Number = 50
+            }
+
+        };
+    }
+}
---
+++ MarioProvaFW/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.OData;
+
+namespace MarioProvaFW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rightUpdObj = new MyObject
+            {
+                Id = 1,
+                Name = "Dario",
+                Surname = "Capocci",
+                Number = 33
+            };
+            var partialUpdObj = new MyObject
+            {
+                Id = 3,
+                Name = "Mario",
+            };
+
+            var list = MyList.List;
+
+            var oldObj1 = list.Where(x => x.Id == rightUpdObj.Id).FirstOrDefault();
+            var oldObj2 = list.Where(x => x.Id == partialUpdObj.Id).FirstOrDefault();
+
+            var properties = typeof(MyObject).GetProperties();
+
+            var result = new MyObject();
+
+            foreach (var prop in properties)
+            {
+                PropertyInfo propertyInfo = rightUpdObj.GetType().GetProperty(prop.Name);
+
+                var propUpValue = propertyInfo.GetValue(rightUpdObj);
+                var propOldValue = propertyInfo.GetValue(oldObj1);
+                
+                if (propUpValue == propOldValue)
+                {
+                    propertyInfo.SetValue(result, propOldValue);
+                    continue;
+                }
+                if (propUpValue.Equals(propOldValue))
+                {
+                    propertyInfo.SetValue(result, propOldValue);
+                    continue;
+                }
+
+                propertyInfo.SetValue(result, propUpValue);
+            }
+
+            return;
+        }
+    }
+}
